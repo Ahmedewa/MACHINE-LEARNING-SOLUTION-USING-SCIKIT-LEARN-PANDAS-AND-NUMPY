b8c4994256eb813799e0ae9bdb300122a69a954c
Ahmedewa <146005021+Ahmedewa@users.noreply.github.com>
2025-08-11 09:04:57 +0100

    Create   src/content/dependancy-conflict.py

---
+++ de[pendancies-conflict.py
@@ -0,0 +1,13 @@
+               DEPENDANCIES CONFLITS
+
+#### **Dependency Conflicts**
+- **Problem**: Conflicting versions of libraries when building ML projects.
+- **Solution**:
+  - Pin library versions in our `requirements.txt`.
+  - Example:
+    ```plaintext
+    tensorflow==2.10.0
+    numpy==1.21.6
+    pandas==1.3.5
+    ```
+
